69adce99610f7dfb6040483d970ae2272db715dd
Damian Rouson <rouson@lbl.gov>
2025-03-17 20:37:26 -0700

    repro(string): rm all but 2 dummy arguments

--- all.f90
+++ all.f90
@@ -4,26 +4,22 @@
     character(len=:), allocatable :: string_
   end type
 
-  type(string_t) array(1)
-
-  array(1)%string_ = "do"
-  print *, equals(bracket(array), [new_string("[do]")])
+  print *, equals(bracket(), [new_string()])
 
 contains
 
-  type(string_t) function new_string(string)
-    character(len=*) string
-    new_string%string_ = string
+  type(string_t) function new_string()
+    new_string%string_ = "["
   end function
 
   logical elemental function equals(lhs, rhs)
-    class(string_t), intent(in) :: lhs, rhs
+    type(string_t), intent(in) :: lhs
+    class(string_t), intent(in) :: rhs
     equals = lhs%string_ == rhs%string_
   end function
 
-  type(string_t) elemental function bracket(self)
-    type(string_t), intent(in) :: self
-    bracket%string_ = "[" // self%string_ // "]"
+  type(string_t) elemental function bracket()
+    bracket%string_ = "["
   end function
 
 end
