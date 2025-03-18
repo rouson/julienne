31b5750da40007390660ad171f817dae4478495f
Damian Rouson <rouson@lbl.gov>
2025-03-17 19:12:51 -0700

    repro(string): reduce dimension of test array

--- test/main.F90
+++ test/main.F90
@@ -1,6 +1,6 @@
   use julienne_string_m, only : string_t
   implicit none
-  type(string_t), allocatable :: array(:)
-  array = string_t(["do", "re", "mi"])
-  print *, array%bracket()        == [string_t("[do]"), string_t("[re]"), string_t("[mi]")]
+  type(string_t) array(1)
+  array = string_t(["do"])
+  print *, array%bracket() == [string_t("[do]")]
 end
