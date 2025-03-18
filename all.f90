f3a27c903d7b1b88da4c5c47619dd40addd6406a
Damian Rouson <rouson@lbl.gov>
2025-03-17 20:31:59 -0700

    repro(string): simplify procedure signatures

--- all.f90
+++ all.f90
@@ -7,27 +7,23 @@
   type(string_t) array(1)
 
   array(1)%string_ = "do"
-  print *, string_t_eq_string_t(bracket(array), [from_characters("[do]")])
+  print *, equals(bracket(array), [new_string("[do]")])
 
 contains
 
-  function from_characters(string) result(new_string)
-    character(len=*), intent(in) :: string
-    type(string_t) new_string
+  type(string_t) function new_string(string)
+    character(len=*) string
     new_string%string_ = string
   end function
 
-  elemental function string_t_eq_string_t(lhs, rhs) result(lhs_eq_rhs)
+  logical elemental function equals(lhs, rhs)
     class(string_t), intent(in) :: lhs, rhs
-    logical lhs_eq_rhs
-    lhs_eq_rhs = lhs%string_ == rhs%string_
+    equals = lhs%string_ == rhs%string_
   end function
 
-  elemental function bracket(self) result(bracketed_self)
+  type(string_t) elemental function bracket(self)
     type(string_t), intent(in) :: self
-    type(string_t) bracketed_self
-    bracketed_self = string_t("[" // self%string_ // "]")
+    bracket = string_t("[" // self%string_ // "]")
   end function
 
 end
-
