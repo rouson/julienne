fb84ff88e5182f1957187fbaa1a167ff328daeac
Damian Rouson <rouson@lbl.gov>
2025-03-17 20:18:27 -0700

    repro(string): reduce reproducer to one file

--- all.f90
+++ all.f90
@@ -49,3 +49,9 @@ contains
   end function
 
 end module
+  use julienne_string_m
+  implicit none
+  type(string_t) array(1)
+  array(1)%string_ = "do"
+  print *, string_t_eq_string_t(bracket(array), [from_characters("[do]")])
+end
--- test/main.F90
+++
@@ -1,6 +0,0 @@
-  use julienne_string_m
-  implicit none
-  type(string_t) array(1)
-  array(1)%string_ = "do"
-  print *, string_t_eq_string_t(bracket(array), [from_characters("[do]")])
-end
