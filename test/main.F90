f05e6bf263779c307a7e869f5dde991b8e8a73ed
Damian Rouson <rouson@lbl.gov>
2025-03-17 19:05:06 -0700

    repro(string): combine test module into test main

--- test/main.F90
+++ test/main.F90
@@ -1,6 +1,6 @@
-  use string_test_m ,only : string_test_t
+  use julienne_string_m, only : string_t
   implicit none
-  type(string_test_t) string_test
-  integer :: passes=0, tests=0
-  call string_test%report(passes, tests)
-end 
+  type(string_t), allocatable :: array(:)
+  array = string_t(["do", "re", "mi"])
+  print *, array%bracket()        == [string_t("[do]"), string_t("[re]"), string_t("[mi]")]
+end
