f55c7ff5406dcffe53758cee25ee506092d3fe6a
Damian Rouson <rouson@lbl.gov>
2025-03-17 18:51:31 -0700

    repro(string):rm extends,asserts,getters,c-interop

--- src/julienne_string_m.f90
+++ src/julienne_string_m.f90
@@ -1,8 +1,6 @@
 ! Copyright (c) 2024, The Regents of the University of California and Sourcery Institute
 ! Terms of use are as specified in LICENSE.txt
 module julienne_string_m
-  use assert_m, only : characterizable_t
-  use iso_c_binding, only : c_bool
   implicit none
   
   private
@@ -12,14 +10,13 @@ module julienne_string_m
   public :: operator(.csv.) ! comma-separated values unary operator
   public :: operator(.sv.)  ! separated-values binary operator
 
-  type, extends(characterizable_t) :: string_t
+  type string_t
     private
     character(len=:), allocatable :: string_
   contains
     procedure :: as_character
     generic :: string => as_character
     procedure :: is_allocated
-    procedure :: get_json_key
     procedure :: file_extension
     procedure :: base_name
     procedure :: bracket
@@ -27,24 +24,6 @@ module julienne_string_m
     generic :: operator(/=)   => string_t_ne_string_t, string_t_ne_character, character_ne_string_t
     generic :: operator(==)   => string_t_eq_string_t, string_t_eq_character, character_eq_string_t
     generic :: assignment(= ) => assign_string_t_to_character, assign_character_to_string_t
-    generic :: get_json_value => get_string, get_string_t_array_with_character_key, get_string_t_array_with_string_t_key &
-                                ,get_real, get_real_with_character_key &
-                                ,get_character, get_character_with_character_key &
-                                ,get_logical, get_logical_with_character_key  &
-                                ,get_real_array ,get_real_array_with_character_key &
-                                ,get_integer_array, get_integer_array_with_character_key &
-                                ,get_integer, get_integer_with_character_key &
-                                ,get_double_precision, get_double_precision_with_character_key &
-                                ,get_double_precision_array, get_double_precision_array_with_character_key
-    procedure, private :: get_real, get_real_with_character_key
-    procedure, private :: get_string, get_string_t_array_with_character_key, get_string_t_array_with_string_t_key
-    procedure, private :: get_logical, get_logical_with_character_key
-    procedure, private :: get_integer, get_integer_with_character_key
-    procedure, private :: get_real_array, get_real_array_with_character_key
-    procedure, private :: get_integer_array, get_integer_array_with_character_key
-    procedure, private :: get_character, get_character_with_character_key
-    procedure, private :: get_double_precision, get_double_precision_with_character_key
-    procedure, private :: get_double_precision_array, get_double_precision_array_with_character_key
     procedure, private :: string_t_ne_string_t, string_t_ne_character
     procedure, private :: string_t_eq_string_t, string_t_eq_character
     procedure, private :: assign_character_to_string_t
@@ -84,12 +63,6 @@ module julienne_string_m
     elemental module function from_default_logical(b) result(string)
       implicit none
       logical, intent(in) :: b
-      type(string_t) string
-    end function
-
-    elemental module function from_logical_c_bool(b) result(string)
-      implicit none
-      logical(c_bool), intent(in) :: b
       type(string_t) string
     end function
 
@@ -183,12 +156,6 @@ module julienne_string_m
       logical string_allocated
     end function
 
-    elemental module function get_json_key(self) result(unquoted_key)
-     implicit none
-      class(string_t), intent(in) :: self
-      type(string_t) unquoted_key
-    end function
-
     elemental module function file_extension(self) result(extension)
       !! result contains all characters in file_name after the last dot (.)
       class(string_t), intent(in) :: self
@@ -199,146 +166,6 @@ module julienne_string_m
       !! result contains all characters in file_name before the last dot (.)
       class(string_t), intent(in) :: self
       type(string_t) base
-    end function
-
-    pure module function get_real(self, key, mold) result(value_)
-      implicit none
-      class(string_t), intent(in) :: self, key
-      real, intent(in) :: mold
-      real value_
-    end function
-
-    pure module function get_real_with_character_key(self, key, mold) result(value_)
-      implicit none
-      class(string_t), intent(in) :: self
-      character(len=*), intent(in) :: key
-      real, intent(in) :: mold
-      real value_
-    end function
-
-    pure module function get_double_precision(self, key, mold) result(value_)
-      implicit none
-      class(string_t), intent(in) :: self, key
-      double precision, intent(in) :: mold
-      double precision value_
-    end function
-
-    pure module function get_double_precision_with_character_key(self, key, mold) result(value_)
-      implicit none
-      class(string_t), intent(in) :: self
-      character(len=*), intent(in) :: key
-      double precision, intent(in) :: mold
-      double precision value_
-    end function
-
-    pure module function get_double_precision_array(self, key, mold) result(value_)
-      implicit none
-      class(string_t), intent(in) :: self, key
-      double precision, intent(in) :: mold(:)
-      double precision, allocatable :: value_(:)
-    end function
-
-    pure module function get_double_precision_array_with_character_key(self, key, mold) result(value_)
-      implicit none
-      class(string_t), intent(in) :: self
-      character(len=*), intent(in) :: key
-      double precision, intent(in) :: mold(:)
-      double precision, allocatable :: value_(:)
-    end function
-
-    pure module function get_character(self, key, mold) result(value_)
-      implicit none
-      class(string_t), intent(in) :: self, key
-      character(len=*), intent(in) :: mold
-      character(len=:), allocatable :: value_
-    end function
-
-    pure module function get_character_with_character_key(self, key, mold) result(value_)
-      implicit none
-      class(string_t), intent(in) :: self
-      character(len=*), intent(in) :: key, mold
-      character(len=:), allocatable :: value_
-    end function
-
-    elemental module function get_string(self, key, mold) result(value_)
-      implicit none
-      class(string_t), intent(in) :: self, key, mold
-      type(string_t) :: value_
-    end function
-
-    pure module function get_string_t_array_with_string_t_key(self, key, mold) result(value_)
-      implicit none
-      class(string_t), intent(in) :: self
-      type(string_t), intent(in) :: key, mold(:)
-      type(string_t), allocatable :: value_(:)
-    end function
-
-    pure module function get_string_t_array_with_character_key(self, key, mold) result(value_)
-      implicit none
-      class(string_t), intent(in) :: self
-      character(len=*), intent(in) :: key
-      type(string_t), intent(in) :: mold(:)
-      type(string_t), allocatable :: value_(:)
-    end function
-
-    pure module function get_integer_with_character_key(self, key, mold) result(value_)
-      implicit none
-      class(string_t), intent(in) :: self
-      character(len=*), intent(in) :: key
-      integer, intent(in) ::  mold
-      integer value_
-    end function
-
-    pure module function get_integer(self, key, mold) result(value_)
-      implicit none
-      class(string_t), intent(in) :: self, key
-      integer, intent(in) ::  mold
-      integer value_
-    end function
-
-    pure module function get_logical_with_character_key(self, key, mold) result(value_)
-      implicit none
-      class(string_t), intent(in) :: self
-      character(len=*), intent(in) :: key
-      logical, intent(in) :: mold
-      logical value_
-    end function
-
-    elemental module function get_logical(self, key, mold) result(value_)
-      implicit none
-      class(string_t), intent(in) :: self, key
-      logical, intent(in) :: mold
-      logical value_
-    end function
-
-    pure module function get_integer_array_with_character_key(self, key, mold) result(value_)
-      implicit none
-      class(string_t), intent(in) :: self
-      character(len=*), intent(in) :: key
-      integer, intent(in) :: mold(:)
-      integer, allocatable :: value_(:)
-    end function
-
-    pure module function get_integer_array(self, key, mold) result(value_)
-      implicit none
-      class(string_t), intent(in) :: self, key
-      integer, intent(in) :: mold(:)
-      integer, allocatable :: value_(:)
-    end function
-
-    pure module function get_real_array_with_character_key(self, key, mold) result(value_)
-      implicit none
-      class(string_t), intent(in) :: self
-      character(len=*), intent(in) :: key
-      real, intent(in) :: mold(:)
-      real, allocatable :: value_(:)
-    end function
-
-    pure module function get_real_array(self, key, mold) result(value_)
-      implicit none
-      class(string_t), intent(in) :: self, key
-      real, intent(in) :: mold(:)
-      real, allocatable :: value_(:)
     end function
 
     elemental module function string_t_eq_string_t(lhs, rhs) result(lhs_eq_rhs)
