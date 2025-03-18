444969e954989207c14e5016a3be28574d75cfe8
Damian Rouson <rouson@lbl.gov>
2025-03-17 19:15:19 -0700

    repro(string): reduce constructor invocations to 1

--- test/main.F90
+++ test/main.F90
@@ -1,6 +1,6 @@
   use julienne_string_m, only : string_t
   implicit none
   type(string_t) array(1)
-  array = string_t(["do"])
+  array(1) = "do"
   print *, array%bracket() == [string_t("[do]")]
 end
