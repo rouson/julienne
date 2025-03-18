d5c29010acad863b237c5d8c7fe72eeab7201c1d
Damian Rouson <rouson@lbl.gov>
2025-03-17 20:54:30 -0700

    repro(string): renamings

--- all.f90
+++ all.f90
@@ -4,18 +4,18 @@
     character(len=:), allocatable :: string_
   end type
 
-  print *, true(string_t("["), [new_string()])
+  print *, always_true(string_t(""), [some_string()])
 
 contains
 
-  type(string_t) function new_string()
-    new_string%string_ = "["
+  type(string_t) function some_string()
+    some_string%string_ = ""
   end function
 
-  logical elemental function true(lhs, rhs)
+  logical elemental function always_true(lhs, rhs)
     type(string_t), intent(in) :: lhs
     class(string_t), intent(in) :: rhs
-    true = .true.
+    always_true = .true.
   end function
 
 end
