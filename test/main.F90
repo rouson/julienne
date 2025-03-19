92e39a06dc86eb5e7bb484dbe30eb5330e283ff5
Damian Rouson <rouson@lbl.gov>
2025-03-18 23:00:30 -0700

    repro: simplify test main

--- test/main.F90
+++ test/main.F90
@@ -1,19 +1,14 @@
-  use julienne_m,only : vector_test_description_t, string_t, test_diagnosis_t, diagnosis_function_i, test_description_t
+  use julienne_m,only : vector_test_description_t, string_t, test_diagnosis_t
   implicit none
 
-  associate(vector_test_descriptions => [vector_test_description_t([string_t("")], check_substring_search)])
+  associate(vector_test_description => vector_test_description_t([string_t::], diagnoses))
   end associate
 
 contains
 
-   function check_substring_search() result(diagnoses)
+   function diagnoses()
     type(test_diagnosis_t), allocatable :: diagnoses(:)
-    type(test_description_t) some_test
-    procedure(diagnosis_function_i), pointer :: unused
-
-    unused => null()
-    some_test = test_description_t("do something", unused)
-    diagnoses = [test_diagnosis_t(test_passed = some_test%contains_text("something"), diagnostics_string="")] 
+    diagnoses = [test_diagnosis_t::] 
   end function
 
 end
