1fd9d7fc62007b18cf191bdc825dbda149a765e9
Damian Rouson <rouson@lbl.gov>
2025-03-18 22:52:08 -0700

    repro: rm vector_test_description_test

--- test/main.F90
+++ test/main.F90
@@ -1,6 +1,21 @@
-  use vector_test_description_test_m ,only : vector_test_description_test_t
+  use julienne_m,only : vector_test_description_t, string_t, test_diagnosis_t, diagnosis_function_i, test_description_t
   implicit none
-  type(vector_test_description_test_t) vector_test_description_test
-  associate(results => vector_test_description_test%results())
-  end associate
+  type(vector_test_description_t), allocatable :: vector_test_descriptions(:)
+
+  vector_test_descriptions = [vector_test_description_t([string_t(""),string_t("")], check_substring_search)]
+
+contains
+   function check_substring_search() result(diagnoses)
+    type(test_diagnosis_t), allocatable :: diagnoses(:)
+    type(test_description_t) doing_something
+    procedure(diagnosis_function_i), pointer :: unused
+
+    unused => null()
+
+    doing_something = test_description_t("doing something", unused)
+    diagnoses = [ & 
+       test_diagnosis_t(test_passed =       doing_something%contains_text("something"),    diagnostics_string="expected .true.") &
+      ,test_diagnosis_t(test_passed = .not. doing_something%contains_text("missing text"), diagnostics_string="expected .true.") &
+    ]   
+  end function
 end
