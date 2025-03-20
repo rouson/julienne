d096bb353e066ed9f1db5bc7b5a1eb5dce8dcbc5
Damian Rouson <rouson@lbl.gov>
2025-03-19 17:58:51 -0700

    repro: mk description component a scalar

--- all.f90
+++ all.f90
@@ -4,11 +4,11 @@
   end type
 
   type vector_test_description_t
-    type(string_t), allocatable :: descriptions_(:)
+    type(string_t), allocatable :: description_
     procedure(diagnoses), pointer, nopass :: vector_diagnosis_function_
   end type
 
-  associate(vector_test_description => construct_from_strings([string_t::], diagnoses))
+  associate(vector_test_description => construct_from_strings(string_t(), diagnoses))
   end associate
 
 contains
@@ -18,11 +18,11 @@ contains
     diagnoses = [logical::]
   end function
 
-  function construct_from_strings(descriptions, vector_diagnosis_function) result(vector_test_description)
-    type(string_t), intent(in) :: descriptions(:)
+  function construct_from_strings(description, vector_diagnosis_function) result(vector_test_description)
+    type(string_t), intent(in) :: description
     procedure(diagnoses), intent(in), pointer :: vector_diagnosis_function
     type(vector_test_description_t) vector_test_description
-    vector_test_description%descriptions_ = descriptions
+    vector_test_description%description_ = description
     vector_test_description%vector_diagnosis_function_ => vector_diagnosis_function
   end function
 
