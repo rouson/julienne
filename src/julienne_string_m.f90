48ca435e88ff9a1b332a4dee84b9cdc48d4e8f36
Damian Rouson <rouson@lbl.gov>
2025-03-19 17:33:13 -0700

    repro: rm all string_t procedures

--- src/julienne_string_m.f90
+++ src/julienne_string_m.f90
@@ -1,78 +1,8 @@
-! Copyright (c) 2024, The Regents of the University of California and Sourcery Institute
-! Terms of use are as specified in LICENSE.txt
 module julienne_string_m
-  use iso_c_binding, only : c_bool
   implicit none
   
-  private
-  public :: string_t
-  public :: array_of_strings
-
   type string_t
-    private
     character(len=:), allocatable :: string_
   end type
 
-  interface string_t
-
-    elemental module function from_characters(string) result(new_string)
-      implicit none
-      character(len=*), intent(in) :: string
-      type(string_t) new_string
-    end function
-
-    elemental module function from_default_integer(i) result(string)
-      implicit none
-      integer, intent(in) :: i
-      type(string_t) string
-    end function
-
-    elemental module function from_default_real(x) result(string)
-      implicit none
-      real, intent(in) :: x
-      type(string_t) string
-    end function
-
-    elemental module function from_double_precision(x) result(string)
-      implicit none
-      double precision, intent(in) :: x
-      type(string_t) string
-    end function
-
-    elemental module function from_default_logical(b) result(string)
-      implicit none
-      logical, intent(in) :: b
-      type(string_t) string
-    end function
-
-    elemental module function from_logical_c_bool(b) result(string)
-      implicit none
-      logical(c_bool), intent(in) :: b
-      type(string_t) string
-    end function
-
-    elemental module function from_default_complex(z) result(string)
-      implicit none
-      complex, intent(in) :: z
-      type(string_t) string
-    end function
-
-    elemental module function from_double_precision_complex(z) result(string)
-      implicit none
-      complex(kind=kind(1D0)), intent(in) :: z
-      type(string_t) string
-    end function
-
-  end interface
-
-  interface
-
-    pure module function array_of_strings(delimited_strings, delimiter) result(strings_array)
-      implicit none
-      character(len=*), intent(in) :: delimited_strings, delimiter
-      type(string_t), allocatable :: strings_array(:)
-    end function
-
-  end interface
-  
 end module julienne_string_m
