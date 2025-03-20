ea6dad093c80c3bb56961b5fb16861489310fa3c
Damian Rouson <rouson@lbl.gov>
2025-03-19 18:16:41 -0700

    repro: rm explicit result declaration

--- all.f90
+++ all.f90
@@ -8,7 +8,7 @@
     procedure(diagnoses), pointer, nopass :: diagnosis_function_
   end type
 
-  associate(test_description => construct_from_strings(string_t(), diagnoses))
+  associate(test_description => test_description(string_t(), diagnoses))
   end associate
 
 contains
@@ -18,10 +18,9 @@ contains
     diagnoses = .true.
   end function
 
-  function construct_from_strings(description, diagnosis_function) result(test_description)
+  type(test_description_t) function test_description(description, diagnosis_function)
     type(string_t), intent(in) :: description
     procedure(diagnoses), intent(in), pointer :: diagnosis_function
-    type(test_description_t) test_description
     test_description%description_ = description
     test_description%diagnosis_function_ => diagnosis_function
   end function
