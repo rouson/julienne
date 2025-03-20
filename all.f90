a709654ed2d53d67c5dd682e7afa5fe3468aadc9
Damian Rouson <rouson@lbl.gov>
2025-03-19 18:49:31 -0700

    repro: obfuscate names

--- all.f90
+++ all.f90
@@ -1,18 +1,18 @@
   implicit none
 
-  type test_description_t
+  type foo_t
     integer, allocatable :: i_
-    procedure(diagnosis), pointer, nopass :: diagnosis_function_
+    procedure(f), pointer, nopass :: f_
   end type
 
-  associate(test_description => test_description_t(0,diagnosis))
+  associate(foo => foo_t(1,f))
   end associate
 
 contains
 
-  function diagnosis()
-    logical, allocatable :: diagnosis
-    diagnosis = .true.
+  function f()
+    logical, allocatable :: f
+    f = .true.
   end function
 
 end
